242c047b0d5cc7a69a9f323ef1c10c13841866cc
rakib <mdrakibhasan240@gmail.com>
2022-06-22 15:47:52 +0600

    Slider-section fix

--- src/components/homecomponent/slider/Subscribe.js
+++ src/components/homecomponent/slider/Subscribe.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Subscribe() {
   return (
     <div>
-        <div className='xl:flex xm:grid xm:grid-flow-row lg:grid-cols-2 md:grid-cols-3 md:grid-flow-row sm:grid-cols-3 xm:grid-cols-2 lg:justify-center md:mt-20 sm:my-8 xm:my-8'>
+        <div className='xl:flex xm:grid xm:grid-flow-row lg:grid-cols-2 md:grid-cols-3 md:grid-flow-row sm:grid-cols-3 xm:grid-cols-2 lg:justify-center md:mt-0 sm:my-8 xm:my-8'>
             <div className='2xl:pr-8 xl:pr-4 xl:border-r border-[#434955] lg:border-0 xl:mb-0 lg:mb-2'>
                 <h4 className='subscribe-text 2xl:text-[36px] xl:text-[28px] md:text-[22px] sm:text-[20px] xm:text-[18px]'>13k <span className='ml-4 text-blue-500 font-medium md:text-[18px] sm:text-[16px]'>Subscriber</span></h4>
             </div>
