22cfcefe281e926f15cf9481669fc7f42d3936bd
rakib <mdrakibhasan240@gmail.com>
2022-07-09 16:51:47 +0600

    auto countdown set

--- src/components/homecomponent/slider/AntqCards.js
+++ src/components/homecomponent/slider/AntqCards.js
@@ -1,23 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { GiSelfLove } from "react-icons/gi";
 import { BsThreeDotsVertical } from "react-icons/bs";
-import { ToastContainer } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
 
 function  AntqCards(props) {
-
-  // const save = () => {
-  //   toast.success("Saved Item.", {
-  //     position: "top-center",
-  //     autoClose: 2000,
-  // });
-  // }
-  // const deleted = () => {
-  //   toast.error("Delete Item.", {
-  //     position: "top-center",
-  //     autoClose: 2000,
-  // });
-  // }
 
   const [dotsActive, setDotsActive] = useState(false);
   const [fill, setFill]  = useState (false);
@@ -71,45 +56,43 @@ function  AntqCards(props) {
 
   return (
     <>
-        <div className='xl:w-[270px] lg:w-full md:w-[260px] bg-[#2A2E3F] rounded-[30px] h-full p-3 nth-position'>
-            <div className=' bg-no-repeat bg-contain bg-center w-full xl:h-[220px] lg:h-[172px] md:h-[212px] sm:h-[188px] xm:h-[144px] relative' style={{ backgroundImage: `url(${props.bgImage})` }}>
-            <div className='absolute right-2 top-4 text-white z-20'>
+      <div className='xl:w-[270px] lg:w-full md:w-[260px] bg-[#2A2E3F] rounded-[30px] h-full p-3 nth-position'>
+        <div className=' bg-no-repeat bg-contain bg-center w-full xl:h-[220px] lg:h-[172px] md:h-[212px] sm:h-[188px] xm:h-[144px] relative' style={{ backgroundImage: `url(${props.bgImage})` }}>
+          <div className='absolute right-2 top-4 text-white z-20'>
             <button onClick={()=> setDotsActive(!dotsActive)} className='font-bold cursor-pointer text-[24px]'><BsThreeDotsVertical /></button>
             <ul className={dotsActive?'dots active-dots duration-200': 'dots'}>
               <li className='mb-1 px-2 py-1 hover:bg-[#7D68F7] rounded-md hover:text-white'><button>Saved</button></li>
               <li className=' px-2 py-1 hover:bg-[#f00] rounded-md hover:text-white'><button>Delete</button></li>
             </ul>
+          </div>
+          <div className='text-center grid grid-flow-row grid-cols-4 relative h-full mx-auto items-end text-white px-4 bottom-2'>
+            <div>
+              <div className='font-bold text-[18px]'>{state.days || '0'}</div>
+              <small>Days</small>
             </div>
-              <div className='text-center grid grid-flow-row grid-cols-4 relative h-full mx-auto items-end text-white px-4 bottom-2'>
-                <div>
-                  <div className='font-bold text-[18px]'>{state.days || '0'}</div>
-                  <small>Days</small>
-                </div>
-                <div>
-                  <div className='font-bold text-[18px]'>{state.hours || '00'}</div>
-                  <small>Hou</small>
-                </div>
-                <div>
-                  <div className='font-bold text-[18px]'>{state.minutes || '00'}</div>
-                  <small>Min</small>
-                </div>
-                <div>
-                  <div className='font-bold text-[18px]'>{state.seconds || '00'}</div>
-                  <small>Sec</small>
-                </div>
-              </div>
+            <div>
+              <div className='font-bold text-[18px]'>{state.hours || '00'}</div>
+              <small>Hou</small>
             </div>
-            <div className='flex justify-between items-center mt-4 md:px-5 sm:px-1 xm:px-1'>
-                <b className='text-white sm:text-[18px] xm:text-[16px]'>{props.title}</b>
-                <span className='text-[#566FFE]'>{props.rate}</span>
+            <div>
+              <div className='font-bold text-[18px]'>{state.minutes || '00'}</div>
+              <small>Min</small>
             </div>
-            <div className='flex justify-between mt-1 md:px-5 sm:px-1 xm:px-1 pb-2'>
-                <span className='text-[#7D8694] sm:text-[16px] xm:text-[14px]'>{props.dis}</span>
-                <span onClick={likeBtn} className='text-[#7D8694] flex items-center'> <GiSelfLove onClick={()=> setFill(!fill)} className={fill ? "mr-2 cursor-pointer text-red-600":"mr-2 cursor-pointer"} />{like}</span>
+            <div>
+              <div className='font-bold text-[18px]'>{state.seconds || '00'}</div>
+              <small>Sec</small>
             </div>
-        <ToastContainer />
-
+          </div>
         </div>
+          <div className='flex justify-between items-center mt-4 md:px-5 sm:px-1 xm:px-1'>
+              <b className='text-white sm:text-[18px] xm:text-[16px]'>{props.title}</b>
+              <span className='text-[#566FFE]'>{props.rate}</span>
+          </div>
+          <div className='flex justify-between mt-1 md:px-5 sm:px-1 xm:px-1 pb-2'>
+              <span className='text-[#7D8694] sm:text-[16px] xm:text-[14px]'>{props.dis}</span>
+              <span onClick={likeBtn} className='text-[#7D8694] flex items-center'> <GiSelfLove onClick={()=> setFill(!fill)} className={fill ? "mr-2 cursor-pointer text-red-600":"mr-2 cursor-pointer"} />{like}</span>
+          </div>
+      </div>
     </>
   )
 }
--- src/components/homecomponent/slider/Subscribe.js
+++ src/components/homecomponent/slider/Subscribe.js
@@ -1,58 +1,19 @@
 import React from 'react'
+import CountUp from 'react-countup';
 
 function Subscribe() {
-
-    // const [count, setCount] = useState(null);
-
-    // const ref = useRef(0);
-
-    // const countdown =(13);
-
-    // const accountertcounter = () => {
-    //     if(ref.current < countdown ) {
-    //         const countresult  = Math.ceil(ref.current + countdown);
-    //         if(countresult > countdown) return setCount(countdown);
-    //         setCount(countresult);
-    //         ref.current =countresult;
-    //     };
-    //     setTimeout(accountertcounter, 5);
-    // };
-
-    // useEffect(() => {
-    //     let inMounted = true;
-    //     if(inMounted) {
-    //         accountertcounter();
-    //     };
-    //     return () => (inMounted = false);
-
-    // }, [countdown]);
-
-    const $counters = document.querySelectorAll(".js-count-up"),
-  options = {
-    useEasing: true,
-    useGrouping: true,
-    separator: ",",
-    decimal: "." };
-
-
-  $counters.forEach(item => {
-    const value = item.dataset.value;
-    const counter =(item, value, options);
-    counter.start();
-  });
-
 
   return (
     <div>
         <div className='xl:flex xm:grid xm:grid-flow-row lg:grid-cols-2 md:grid-cols-3 md:grid-flow-row sm:grid-cols-3 xm:grid-cols-2 lg:justify-center xl:mt-20 md:mt-0 sm:my-8 xm:my-8'>
             <div className='2xl:pr-8 xl:pr-4 xl:border-r border-[#434955] lg:border-0 xl:mb-0 lg:mb-2'>
-                <h4 data-value='13' className='js-count-up subscribe-text 2xl:text-[36px] xl:text-[28px] md:text-[22px] sm:text-[20px] xm:text-[18px]'>13k <span className='ml-4 text-blue-500 font-medium md:text-[18px] sm:text-[16px]'>Subscriber</span></h4>
+                <h4 data-value='13' className='js-count-up subscribe-text 2xl:text-[36px] xl:text-[28px] md:text-[22px] sm:text-[20px] xm:text-[18px]'><CountUp end={13} duration={1.5} /> k <span className='ml-4 text-blue-500 font-medium md:text-[18px] sm:text-[16px]'>Subscriber</span></h4>
             </div>
             <div className='2xl:px-8 xl:px-4  xl:border-r border-[#434955] lg:border-0 xl:mb-0 lg:mb-2'>
-                <h4 className='subscribe-text 2xl:text-[36px] xl:text-[28px] md:text-[22px] sm:text-[20px] xm:text-[18px]'>33k <span className='ml-4 text-blue-500 font-medium md:text-[18px] sm:text-[16px]'>Subscriber</span></h4>
+                <h4 className='subscribe-text 2xl:text-[36px] xl:text-[28px] md:text-[22px] sm:text-[20px] xm:text-[18px]'><CountUp end={33} duration={1.5} /> k <span className='ml-4 text-blue-500 font-medium md:text-[18px] sm:text-[16px]'>Subscriber</span></h4>
             </div>
             <div className='2xl:ml-8 xl:ml-4'>
-                <h4 className='subscribe-text 2xl:text-[36px] xl:text-[28px] md:text-[22px] sm:text-[20px] xm:text-[18px]'>16k+ <span className='ml-4 text-blue-500 font-medium md:text-[18px] sm:text-[16px]'>Subscriber</span></h4>
+                <h4 className='subscribe-text 2xl:text-[36px] xl:text-[28px] md:text-[22px] sm:text-[20px] xm:text-[18px]'><CountUp end={16} duration={1.5} /> k+ <span className='ml-4 text-blue-500 font-medium md:text-[18px] sm:text-[16px]'>Subscriber</span></h4>
             </div>
         </div>
     </div>
